af8cea9d0a8dbeaf87c5fe788395fcfea4dda206
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only video user can modify video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,14 @@
 class VideosController < ApplicationController
+  before_action :current_user_must_be_video_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_video_user
+    video = Video.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == video.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @videos = Video.all
 
