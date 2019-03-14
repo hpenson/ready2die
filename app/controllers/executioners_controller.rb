d727c3f2172a503bc85984e21b1f0db3dc89451d
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only executioner user can modify executioner

--- app/controllers/executioners_controller.rb
+++ app/controllers/executioners_controller.rb
@@ -1,4 +1,14 @@
 class ExecutionersController < ApplicationController
+  before_action :current_user_must_be_executioner_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_executioner_user
+    executioner = Executioner.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == executioner.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @executioners = Executioner.all
 
