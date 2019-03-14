5d4f998574b25309649894b23d87b967869ff216
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:33:30 +0000

    Direct association between friends and connections

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,6 +1,9 @@
 class Connection < ApplicationRecord
   # Direct associations
 
+  belongs_to :bfriend,
+             :class_name => "Friend"
+
   belongs_to :afriend,
              :class_name => "Friend"
 
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,11 @@
 class Friend < ApplicationRecord
   # Direct associations
 
+  has_many   :made_connections,
+             :class_name => "Connection",
+             :foreign_key => "bfriend_id",
+             :dependent => :destroy
+
   has_many   :connections,
              :foreign_key => "afriend_id",
              :dependent => :destroy
