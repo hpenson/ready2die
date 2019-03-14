dd8cc152bdb9942a41d866681d94008cbc7ce719
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:34:18 +0000

    Whitelist columns in Mc for ActiveAdmin

--- app/admin/mcs.rb
+++ app/admin/mcs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Mc do
+
+ permit_params :rank, :guest_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
