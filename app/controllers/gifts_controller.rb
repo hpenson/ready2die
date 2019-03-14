e17bbb14a26b95b3950032241afe47498eae6133
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only gift user can modify gift

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,14 @@
 class GiftsController < ApplicationController
+  before_action :current_user_must_be_gift_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_gift_user
+    gift = Gift.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == gift.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @gifts = Gift.all
 
