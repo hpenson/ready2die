6689170dcbfc3ed14580e68a5a4771f4f25993dc
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:07 +0000

    Only service user can access show page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  before_action :current_user_must_be_service_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_service_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_service_user
     service = Service.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
