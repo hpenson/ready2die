898e34f1d3602ce56fabe8a5a4a97f2f6c31e2e6
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:34:14 +0000

    Whitelist columns in Speaker for ActiveAdmin

--- app/admin/speakers.rb
+++ app/admin/speakers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Speaker do
+
+ permit_params :rank, :guest_id, :position, :description
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
