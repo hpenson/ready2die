1d1401839e6a396b30c0a7a006769a55df97f2d7
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:33:22 +0000

    Direct association between users and friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :friends,
+             :dependent => :destroy
+
   has_many   :remains,
              :dependent => :destroy
 
