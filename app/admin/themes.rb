881a7f5540e16f2572e70783654fb6070c936b56
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:34:02 +0000

    Whitelist columns in Theme for ActiveAdmin

--- app/admin/themes.rb
+++ app/admin/themes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Theme do
+
+ permit_params :rank
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
