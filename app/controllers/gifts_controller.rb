6f00c232ce34db62194f6423382111dbe93aa3f5
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only gift user can access show page

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,5 @@
 class GiftsController < ApplicationController
-  before_action :current_user_must_be_gift_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_gift_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_gift_user
     gift = Gift.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
