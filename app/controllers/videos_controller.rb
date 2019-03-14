a0805589d67e04c5d641a25006114e8d2b4698c4
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only video user can access show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  before_action :current_user_must_be_video_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_video_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_video_user
     video = Video.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
