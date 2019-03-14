5f0653cdc6ef778e3c130555d8d3cf3524b6ed04
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:33:22 +0000

    Direct association between users and discretions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :discretions,
+             :dependent => :destroy
+
   has_many   :messages,
              :dependent => :destroy
 
