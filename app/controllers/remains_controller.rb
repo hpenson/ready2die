9974f1450de8e1d369cb9b05702d57e2332c1e21
Raghu Betina <raghu@firstdraft.com>
2019-03-14 13:36:08 +0000

    Only remain user can modify remain

--- app/controllers/remains_controller.rb
+++ app/controllers/remains_controller.rb
@@ -1,4 +1,14 @@
 class RemainsController < ApplicationController
+  before_action :current_user_must_be_remain_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_remain_user
+    remain = Remain.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == remain.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @remains = Remain.all
 
